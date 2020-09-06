88ba53b36cfba953e0af9561835a1ddc1a3f8f4d
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-06 22:03:43 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -7,20 +7,24 @@ import Navbar from "./component/Navbar";
 import ProjectBoard from "./component/ProjectBoard";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProjectTask from "./component/ProjectTask/AddProjectTask";
+import { Provider } from "react-redux";
+import store from "store";
 function App() {
     return (
-        <Router>
-            <div className="App">
-                <Navbar />
-                <Route exact path="/" component={ProjectBoard} />
+        <Provider store={store}>
+            <Router>
+                <div className="App">
+                    <Navbar />
+                    <Route exact path="/" component={ProjectBoard} />
 
-                <Route
-                    exact
-                    path="/addProjectTask"
-                    component={AddProjectTask}
-                />
-            </div>
-        </Router>
+                    <Route
+                        exact
+                        path="/addProjectTask"
+                        component={AddProjectTask}
+                    />
+                </div>
+            </Router>
+        </Provider>
     );
 }
 
