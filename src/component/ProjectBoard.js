3e0abb31b52e5b24e114194c3f67b6b6a10f265f
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-08 15:09:50 +0100

    wip

--- src/component/ProjectBoard.js
+++ src/component/ProjectBoard.js
@@ -43,10 +43,51 @@ class ProjectBoard extends Component {
                         doneItems.push(tasks[i]);
                     }
                 }
+                return (
+                    <React.Fragment>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-4">
+                                    <div className="card text-center mb-2">
+                                        <div className="card-header bg-secondary text-white">
+                                            <h3>TO DO</h3>
+                                        </div>
+                                    </div>
+
+                                    {/* <!-- SAMPLE PROJECT TASK STARTS HERE --> */}
+                                    {/* <ProjectTaskItem /> */}
+                                    {todoItems}
+
+                                    {/* <!-- SAMPLE PROJECT TASK ENDS HERE --> */}
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="card text-center mb-2">
+                                        <div className="card-header bg-primary text-white">
+                                            <h3>In Progress</h3>
+                                        </div>
+                                    </div>
+                                    {/* <!-- SAMPLE PROJECT TASK STARTS HERE --> */}
+                                    {inProgressItems}
+
+                                    {/* <!-- SAMPLE PROJECT TASK ENDS HERE --> */}
+                                    {/* <ProjectTaskItem /> */}
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="card text-center mb-2">
+                                        <div className="card-header bg-success text-white">
+                                            <h3>Done</h3>
+                                        </div>
+                                    </div>
+                                    {doneItems}
+                                </div>
+                            </div>
+                        </div>
+                    </React.Fragment>
+                );
             }
         };
 
-        BoardAlgorithm(project_tasks);
+        BoardContent = BoardAlgorithm(project_tasks);
 
         return (
             <div>
@@ -59,46 +100,7 @@ class ProjectBoard extends Component {
                     </Link>
                     <br />
                     <hr />
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-4">
-                                <div className="card text-center mb-2">
-                                    <div className="card-header bg-secondary text-white">
-                                        <h3>TO DO</h3>
-                                    </div>
-                                </div>
-
-                                {/* <!-- SAMPLE PROJECT TASK STARTS HERE --> */}
-                                {/* <ProjectTaskItem /> */}
-                                {todoItems}
-
-                                {/* <!-- SAMPLE PROJECT TASK ENDS HERE --> */}
-                            </div>
-                            <div className="col-md-4">
-                                <div className="card text-center mb-2">
-                                    <div className="card-header bg-primary text-white">
-                                        <h3>In Progress</h3>
-                                    </div>
-                                </div>
-                                {/* <!-- SAMPLE PROJECT TASK STARTS HERE --> */}
-                                {inProgressItems}
-
-                                {/* <!-- SAMPLE PROJECT TASK ENDS HERE --> */}
-                                {/* <ProjectTaskItem /> */}
-                            </div>
-                            <div className="col-md-4">
-                                <div className="card text-center mb-2">
-                                    <div className="card-header bg-success text-white">
-                                        <h3>Done</h3>
-                                    </div>
-                                </div>
-                                {/* <!-- SAMPLE PROJECT TASK STARTS HERE --> */}
-                                {doneItems}
-
-                                {/* <!-- SAMPLE PROJECT TASK ENDS HERE --> */}
-                            </div>
-                        </div>
-                    </div>
+                    {BoardContent}
                 </div>
             </div>
         );
